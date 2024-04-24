98a863926595a53759ca5e09a6f5e8c97e9e1863
abdylreshit cherkezov <cherkezov@abdylreshits-MacBook-Pro.local>
2024-04-24 11:31:14 +0500

    add request

--- src/Containers/WarehouseSection/Managers/UI/API/Controllers/CreateTreeController.php
+++ src/Containers/WarehouseSection/Managers/UI/API/Controllers/CreateTreeController.php
@@ -2,21 +2,14 @@
 
 namespace App\Containers\WarehouseSection\Managers\UI\API\Controllers;
 
+use App\Containers\WarehouseSection\Managers\UI\API\Requests\CreateTreeRequest;
 use App\Ship\Core\Abstracts\Controllers\ApiController;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 final class CreateTreeController extends ApiController
 {
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(CreateTreeRequest $request): JsonResponse
     {
-        $request->validate([
-            'name' => 'required|string',
-            'model' => 'required|string|in:CATEGORY,WAREHOUSE,REGION',
-            'parent_type' => 'required|string|in:CATEGORY,WAREHOUSE,REGION',
-            'parent_id' => 'nullable|integer',
-        ]);
-
         $manager = getMenuManager();
 
         $manager->createTree($request->all());
---
+++ src/Containers/WarehouseSection/Managers/UI/API/Requests/CreateTreeRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Containers\WarehouseSection\Managers\UI\API\Requests;
+
+use App\Ship\Core\Abstracts\Requests\Request;
+
+class CreateTreeRequest extends Request
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','string'],
+            'model' => ['required','string','in:CATEGORY,WAREHOUSE,REGION'],
+            'parent_type' => ['required','string','in:CATEGORY,WAREHOUSE,REGION'],
+            'parent_id' => ['nullable','integer'],
+        ];
+    }
+}
