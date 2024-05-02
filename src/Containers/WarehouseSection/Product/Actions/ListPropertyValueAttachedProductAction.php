0b2ca2e7bb4ba429e8421d8ca69872068b705c92
abdylreshit cherkezov <cherkezov@abdylreshits-MacBook-Pro.local>
2024-05-02 14:48:58 +0500

    add property mock data

--- src/Containers/WarehouseSection/Product/Actions/ListPropertyValueAttachedProductAction.php
+++ src/Containers/WarehouseSection/Product/Actions/ListPropertyValueAttachedProductAction.php
@@ -2,14 +2,14 @@
 
 namespace App\Containers\WarehouseSection\Product\Actions;
 
-use App\Containers\WarehouseSection\Product\Models\PropertyValue;
+use App\Containers\WarehouseSection\Product\Models\Property;
 use App\Ship\Core\Abstracts\Actions\Action;
 
 class ListPropertyValueAttachedProductAction extends Action
 {
     public function handle()
     {
-        $properties = PropertyValue::with('values')->get();
+        $properties = Property::with('values')->get();
 
         return $properties;
     }
