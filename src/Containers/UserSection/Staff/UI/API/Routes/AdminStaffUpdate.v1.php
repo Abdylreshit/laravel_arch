10114872ad3de238cd0d74e4f2b99820d0af99aa
abdylreshit cherkezov <cherkezov@abdylreshits-MacBook-Pro.local>
2024-05-23 16:14:39 +0500

    staff edit change post

--- src/Containers/UserSection/Staff/UI/API/Routes/AdminStaffUpdate.v1.php
+++ src/Containers/UserSection/Staff/UI/API/Routes/AdminStaffUpdate.v1.php
@@ -3,7 +3,7 @@
 use App\Containers\UserSection\Staff\UI\API\Controllers\StaffUpdateController;
 use Illuminate\Support\Facades\Route;
 
-Route::put('admin/staff/{id}/update', StaffUpdateController::class)
+Route::post('admin/staff/{id}/update', StaffUpdateController::class)
     ->middleware([
         'auth:admin',
     ])
